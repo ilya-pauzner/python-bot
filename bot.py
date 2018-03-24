a4770f71339fbe3c8fc0a9a20aa1952d93dc4136
ilya-pauzner <ilya-pauzner@yandex.ru>
2018-03-24 16:50:30 +0300

    Fixed README

--- bot.py
+++ bot.py
@@ -1,39 +1,40 @@
-import config
+
 import telebot
 import requests
+
 import os
 import logging
 
-# config is python file
-# now the only variable there is TOKEN, its value is Telegram Bot Token
-
-bot = telebot.TeleBot(config.TOKEN)
-
-logger = telebot.logger
-telebot.logger.setLevel(logging.WARNING)
-
-jobs = 0
-
-@bot.message_handler(content_types = ["document"])
-def handle_job(message):
-	global jobs
-	jobs += 1
-	j = jobs
-	bot.send_message(message.chat.id, "job %d assigned" % (j))
+if __name__ == '__main__':
+	# Telegram bot token
+	TOKEN = open('config', 'r').read()
+	bot = telebot.TeleBot(TOKEN)
 	
-	file_info = bot.get_file(message.document.file_id)
-	file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(config.TOKEN, file_info.file_path))
-	name = 'task_%d.py' % jobs
-	py_file = open(name, 'w')
-	py_file.write(file.text)
-	py_file.close()
-	os.system("python %s" % (name))
+	logger = telebot.logger
+	telebot.logger.setLevel(logging.WARNING)
 	
-	bot.send_message(message.chat.id, "job %d done" % (j))
-
+	jobs = 0
 	
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-	bot.reply_to(message, 'Unknown command')
+	@bot.message_handler(content_types = ["document"])
+	def handle_job(message):
+		global jobs
+		jobs += 1
+		j = jobs
+		bot.send_message(message.chat.id, "job %d assigned" % (j))
+		
+		file_info = bot.get_file(message.document.file_id)
+		file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(config.TOKEN, file_info.file_path))
+		name = 'task_%d.py' % jobs
+		py_file = open(name, 'w')
+		py_file.write(file.text)
+		py_file.close()
+		os.system("python %s" % (name))
+		
+		bot.send_message(message.chat.id, "job %d done" % (j))
 	
-bot.polling()
+		
+	@bot.message_handler(func=lambda m: True)
+	def echo_all(message):
+		bot.reply_to(message, 'Unknown command')
+		
+	bot.polling()
